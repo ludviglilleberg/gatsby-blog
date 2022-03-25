8c0ca41f1cdfe0a64de6f0799e57c3646977f7b3
Ludvig Lilleberg <ludvig.lilleberg@visma.com>
2022-03-25 22:38:22 +0100

    Added word posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = ({ data }) => {
       <Seo title="Home" />
       <div>
         <h1>Ludvig's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} posts</h4>
         {
           data.allMarkdownRemark.edges.map(({node}) =>(
             <div key={node.id}>
